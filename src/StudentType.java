5df1ff22918616045204c8b8a3dfc24a3bd3bc5d
Kamil <stacho688@gmail.com>
2017-03-12 13:05:13 +0100

    Added StudentType enum

---
+++ src/StudentType.java
@@ -0,0 +1,24 @@
+/**
+ * Created by staho on 12.03.2017.
+ */
+public enum StudentType {
+    STATIONARY {
+        @Override
+        public String toString() {
+            return "stationary";
+        }
+    },
+    NONSTATIONARY {
+        @Override
+        public String toString(){
+            return "nonstationary";
+        }
+    },
+    POSTGRADUATE {
+        @Override
+        public  String toString(){
+            return "postgraduate";
+        }
+    }
+}
+
